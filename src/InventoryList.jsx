2efbe06e7ce57dcb726ba8161b018776b9b789f2
rachelkxm@gmail.com <rachelkxm@gmail.com>
2017-08-15 13:24:22 -0700

    reconstruct code

--- src/InventoryList.jsx
+++ src/InventoryList.jsx
@@ -3,42 +3,8 @@ import {topics} from './constants'
 import {getProperties, postProperty, updateProperty, getTopics} from './ServiceCalls';
 import PropertyField from './PropertyField';
 import PropertyTable from './PropertyTable';
+import SearchBar from './SearchBar';
 import './Inventory.css';
-
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-  }
-
-  handleFilterTextInputChange(e) {
-    this.props.onFilterInputChange(e.target.value);
-  }
-
-  handleSelectChange(e) {
-    this.props.onSelectStatusChange(e.target);
-  }
-
-  render() {
-    return (
-      <form className="searchbar">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={this.props.filterText}
-          onChange={this.handleFilterTextInputChange}
-        />
-        <p>
-          <input type="radio" name="status" value="all" checked={this.props.selectAll} onClick={this.handleSelectChange}/>All
-          <input type="radio" name="status" value="onsale" checked={this.props.selectOnSale} onClick={this.handleSelectChange}/>On Sale
-          <input type="radio" name="status" value="pending" checked={this.props.selectPending} onClick={this.handleSelectChange}/>Pending
-          <input type="radio" name="status" value="sold" checked={this.props.selectSold} onClick={this.handleSelectChange}/>Sold
-        </p>
-      </form>
-    );
-  }
-}
 class AddButton extends React.Component {
     render(){
         return(
